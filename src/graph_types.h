32be688da432784c80ee75fe07a9e71b47ac6cb1
Keyan Pishdadian <kpishdadian@gmail.com>
2018-10-10 01:19:44 -0700

    Add type alias for Node/Edge/Weight types

--- src/graph_types.h
+++ src/graph_types.h
@@ -1,38 +1,46 @@
 #ifndef GRAPH_TYPES_H_
 #define GRAPH_TYPES_H_
 #include <cstdint>
+#include <unordered_set>
 #include <vector>
+
+using NodeID = int64_t;
+using EdgeID = int64_t;
+using Weight = int;
+
+using NodeIDList = std::vector<NodeID>;
+using NodeIDSet = std::unordered_set<NodeID>;
 
 // Represents a directed edge between two nodes.
 struct Edge {
-  Edge(int64_t head_node_id, int weight)
+  Edge(NodeID head_node_id, int weight)
       : head_node_osm_id_(head_node_id)
       , weight_(weight) {}
 
-  int64_t head_node_osm_id_;
+  NodeID head_node_osm_id_;
   // Travel time in seconds.
-  int weight_;
+  Weight weight_;
 };
 
 struct Node {
-  Node(int64_t osm_id, double lat, double lng)
+  Node(NodeID osm_id, double lat, double lng)
       : osm_id_(osm_id)
       , lat_(lat)
       , lng_(lng) {}
 
-  int osm_id_;
+  NodeID osm_id_;
   double lat_;
   double lng_;
   std::vector<Edge> outgoing_edges_;
 };
 
 struct WeightedNode {
-  WeightedNode(Node& node, int weight)
+  WeightedNode(Node& node, Weight weight)
       : node_(node)
       , weight_(weight) {}
 
   Node& node_;
-  int weight_;
+  Weight weight_;
 };
 
 struct CompareWeightedNodes {
