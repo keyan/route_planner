6da641c88f3669001d9e8e6261e9220240f2df2f
Keyan Pishdadian <kpishdadian@gmail.com>
2018-10-23 22:45:32 -0700

    Update tests

--- test/dijkstras_test.cc
+++ test/dijkstras_test.cc
@@ -6,6 +6,7 @@
 #include "../src/dijkstras.h"
 #include "../src/graph_types.h"
 #include "../src/road_network.h"
+#include "util/constants.h"
 
 using MinHeapPriorityQueue = std::priority_queue<
     WeightedNode,
@@ -13,6 +14,8 @@ using MinHeapPriorityQueue = std::priority_queue<
     std::greater<std::vector<WeightedNode>::value_type>>;
 
 // Construct a shared graph for testing:
+//
+// 0
 //
 // 1 -> 2
 // |
@@ -24,14 +27,15 @@ using MinHeapPriorityQueue = std::priority_queue<
 RoadNetwork default_road_network() {
   RoadNetwork road_network = RoadNetwork();
 
-  road_network.add_node(1, 0.0, 0.0);
-  road_network.add_node(2, 0.0, 0.0);
-  road_network.add_node(3, 0.0, 0.0);
-  road_network.add_node(4, 0.0, 0.0);
-  road_network.add_node(5, 0.0, 0.0);
-  road_network.add_node(6, 0.0, 0.0);
-  road_network.add_node(7, 0.0, 0.0);
-  road_network.add_node(8, 0.0, 0.0);
+  road_network.add_node(0, 0, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
+  road_network.add_node(1, 1, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
+  road_network.add_node(2, 2, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
+  road_network.add_node(3, 3, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
+  road_network.add_node(4, 4, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
+  road_network.add_node(5, 5, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
+  road_network.add_node(6, 6, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
+  road_network.add_node(7, 7, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
+  road_network.add_node(8, 8, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
 
   road_network.add_edge(1, 2, 1);
   road_network.add_edge(1, 3, 1);
@@ -48,8 +52,8 @@ TEST_CASE("Verify default road_network", "[Dijkstra]") {
   RoadNetwork road_network = default_road_network();
   REQUIRE(
       road_network.as_string() ==
-      "8 14 { 1 (2, 3, 5, ) } { 2 (1, ) } { 3 (1, 4, 5, 6, ) } { 4 (3, ) } { 5 "
-      "(1, 3, ) } { 6 (3, ) } { 7 (8, ) } { 8 (7, ) }");
+      "9 14 { 0 () } { 1 (2, 3, 5, ) } { 2 (1, ) } { 3 (1, 4, 5, 6, ) } { 4 (3,"
+      " ) } { 5 (1, 3, ) } { 6 (3, ) } { 7 (8, ) } { 8 (7, ) }");
 }
 
 TEST_CASE("Min-heap priority queue", "[DS]") {
@@ -70,17 +74,23 @@ TEST_CASE("Search w -1 target marks visited_nodes_", "[Dijkstra]") {
   RoadNetwork road_network = default_road_network();
 
   Dijkstras dijkstra = Dijkstras(road_network);
-  dijkstra.search(1, -1);
+  dijkstra.search(0, -1);
   dijkstra.set_round(1);
+  dijkstra.search(1, -1);
+  dijkstra.set_round(2);
   dijkstra.search(7, -1);
 
-  for (auto it : dijkstra.visited_nodes_) {
-    // Nodes 7 + 8 are disconnected, should be marked as second round
-    if (it.first == 7 or it.first == 8) {
-      REQUIRE(it.second == 1);
+  NodeID i = 0;
+  for (auto it = dijkstra.visited_nodes_.begin();
+       it != dijkstra.visited_nodes_.end(); it++, i++) {
+    // Nodes 7 + 8 are disconnected, should be marked as round 2
+    if (i == 7 or i == 8) {
+      REQUIRE(*it == 2);
+    } else if (i == 0) {
+      REQUIRE(*it == 0);
       // All other nodes are part of the LCC
     } else {
-      REQUIRE(it.second == 0);
+      REQUIRE(*it == 1);
     }
   }
 }
--- test/road_network_test.cc
+++ test/road_network_test.cc
@@ -10,126 +10,129 @@ TEST_CASE("Empty graph representation", "[]") {
   REQUIRE(road_network.as_string() == "0 0");
 }
 
-TEST_CASE("Add nodes", "[]") {
+TEST_CASE("Add nodes", "[road_network]") {
   RoadNetwork road_network = RoadNetwork();
-  road_network.add_node(1, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
-  road_network.add_node(2, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
-  REQUIRE(road_network.as_string() == "2 0 { 1 () } { 2 () }");
+  road_network.add_node(0, 0, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
+  road_network.add_node(1, 1, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
+  REQUIRE(road_network.as_string() == "2 0 { 0 () } { 1 () }");
 }
 
-TEST_CASE("Add edges", "[]") {
+TEST_CASE("Add edges", "[road_network]") {
   RoadNetwork road_network = RoadNetwork();
-  road_network.add_node(1, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
-  road_network.add_node(2, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
+  road_network.add_node(0, 0, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
+  road_network.add_node(1, 1, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
 
-  road_network.add_edge(1, 2, 30);
-  REQUIRE(road_network.as_string() == "2 2 { 1 (2, ) } { 2 (1, ) }");
+  road_network.add_edge(0, 1, 30);
+  REQUIRE(road_network.as_string() == "2 2 { 0 (1, ) } { 1 (0, ) }");
 
-  road_network.add_edge(1, 2, 15);
-  road_network.add_edge(2, 1, 20);
-  road_network.add_edge(2, 1, 10);
+  road_network.add_edge(0, 1, 15);
+  road_network.add_edge(1, 0, 20);
+  road_network.add_edge(1, 0, 10);
   REQUIRE(
       road_network.as_string() ==
-      "2 8 { 1 (2, 2, 2, 2, ) } { 2 (1, 1, 1, 1, ) }");
+      "2 8 { 0 (1, 1, 1, 1, ) } { 1 (0, 0, 0, 0, ) }");
 }
 
-TEST_CASE("Add ways produces correct graph", "[]") {
+TEST_CASE("Add ways produces correct graph", "[road_network]") {
   RoadNetwork road_network = RoadNetwork();
-  road_network.add_node(1, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
-  road_network.add_node(2, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
-  road_network.add_node(3, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
-  REQUIRE(road_network.as_string() == "3 0 { 1 () } { 2 () } { 3 () }");
+  road_network.add_node(0, 0, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
+  road_network.add_node(1, 1, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
+  road_network.add_node(2, 2, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
+  REQUIRE(road_network.as_string() == "3 0 { 0 () } { 1 () } { 2 () }");
 
-  road_network.add_way({1, 2, 3}, "trunk");
-  road_network.add_way({2, 1}, "trunk");
+  road_network.add_way({0, 1, 2}, "trunk");
+  road_network.add_way({1, 2}, "trunk");
   REQUIRE(
       road_network.as_string() ==
-      "3 6 { 1 (2, 2, ) } { 2 (1, 3, 1, ) } { 3 (2, ) }");
+      "3 6 { 0 (1, ) } { 1 (0, 2, 2, ) } { 2 (1, 1, ) }");
 }
 
-TEST_CASE("Add ways produces edges with correct weight", "[]") {
+TEST_CASE("Add ways produces edges with correct weight", "[road_network]") {
   RoadNetwork road_network = RoadNetwork();
 
-  road_network.add_node(1, POINT_1_LAT, POINT_1_LNG);
-  road_network.add_node(2, POINT_2_LAT, POINT_2_LNG);
+  road_network.add_node(0, 0, POINT_1_LAT, POINT_1_LNG);
+  road_network.add_node(1, 1, POINT_2_LAT, POINT_2_LNG);
 
-  road_network.add_way({1, 2}, "service");
+  road_network.add_way({0, 1}, "service");
 
   Weight expected_weight = int(((POINTS_DISTANCE_KM / 5.0) * MS_IN_HOUR) + 0.5);
   REQUIRE(
       std::abs(
-          road_network.graph_.at(1).outgoing_edges_[0].weight_ -
+          road_network.graph_[0].outgoing_edges_[0].weight_ -
           expected_weight) <= EPSILON);
 }
 
-TEST_CASE("Calculate travel time accurate", "[]") {
+TEST_CASE("Calculate travel time accurate", "[road_network]") {
   RoadNetwork road_network = RoadNetwork();
-  road_network.add_node(1, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
-  road_network.add_node(2, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
+  road_network.add_node(0, 0, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
+  road_network.add_node(1, 1, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
 }
 
-TEST_CASE("Reduce to LCC", "[]") {
+TEST_CASE("Reduce to LCC", "[road_network]") {
+  // 0
+  //
   // 1 - 2
   //
   // 3 - 4
   //
   // 5 - 6 - 7
   RoadNetwork road_network = RoadNetwork();
-  road_network.add_node(1, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
-  road_network.add_node(2, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
-  road_network.add_node(3, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
-  road_network.add_node(4, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
-  road_network.add_node(5, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
-  road_network.add_node(6, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
-  road_network.add_node(7, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
+  road_network.add_node(0, 0, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
+  road_network.add_node(1, 1, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
+  road_network.add_node(2, 2, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
+  road_network.add_node(3, 3, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
+  road_network.add_node(4, 4, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
+  road_network.add_node(5, 5, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
+  road_network.add_node(6, 6, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
+  road_network.add_node(7, 7, UNUSED_LAT_LNG, UNUSED_LAT_LNG);
 
   road_network.add_edge(1, 2, 1);
   road_network.add_edge(3, 4, 1);
   road_network.add_edge(5, 6, 1);
   road_network.add_edge(6, 7, 1);
   REQUIRE(
-      road_network.as_string() ==
-      "7 8 { 1 (2, ) } { 2 (1, ) } { 3 (4, ) } { 4 (3, ) } { 5 (6, ) } { 6 (5, "
-      "7, ) } { 7 (6, ) }");
+      road_network.as_string() == "8 8 { 0 () } { 1 (2, ) } { 2 (1, ) } { 3 "
+                                  "(4, ) } { 4 (3, ) } { 5 (6, ) } { 6 (5, "
+                                  "7, ) } { 7 (6, ) }");
 
   road_network.reduce_to_largest_connected_component();
 
   REQUIRE(
-      road_network.as_string() == "3 4 { 5 (6, ) } { 6 (5, 7, ) } { 7 (6, ) }");
+      road_network.as_string() == "3 4 { 0 (1, ) } { 1 (0, 2, ) } { 2 (1, ) }");
 }
 
-TEST_CASE("Load XML OSM file", "[]") {
+TEST_CASE("Load XML OSM file", "[road_network]") {
   RoadNetwork road_network = RoadNetwork();
   road_network.load_from_osm_file("test/data/test.osm");
 
   REQUIRE(
       road_network.as_string() ==
-      "8 14 { 1 (2, 3, 5, ) } { 2 (1, ) } { 3 (1, 4, 5, 6, ) } { 4 (3, ) } { 5 "
-      "(1, 3, ) } { 6 (3, ) } { 7 (8, ) } { 8 (7, ) }");
+      "9 14 { 0 () } { 1 (2, 3, 5, ) } { 2 (1, ) } { 3 (1, 4, 5, 6, ) } { 4 "
+      "(3, ) } { 5 (1, 3, ) } { 6 (3, ) } { 7 (8, ) } { 8 (7, ) }");
 }
 
-TEST_CASE("Reduce XML graph to LCC", "[]") {
+TEST_CASE("Reduce XML graph to LCC", "[road_network]") {
   RoadNetwork road_network = RoadNetwork();
   road_network.load_from_osm_file("test/data/test.osm");
 
   REQUIRE(
       road_network.as_string() ==
-      "8 14 { 1 (2, 3, 5, ) } { 2 (1, ) } { 3 (1, 4, 5, 6, ) } { 4 (3, ) } { 5 "
-      "(1, 3, ) } { 6 (3, ) } { 7 (8, ) } { 8 (7, ) }");
+      "9 14 { 0 () } { 1 (2, 3, 5, ) } { 2 (1, ) } { 3 (1, 4, 5, 6, ) } { 4 "
+      "(3, ) } { 5 (1, 3, ) } { 6 (3, ) } { 7 (8, ) } { 8 (7, ) }");
 
   road_network.reduce_to_largest_connected_component();
 
   REQUIRE(
       road_network.as_string() ==
-      "6 12 { 1 (2, 3, 5, ) } { 2 (1, ) } { 3 (1, 4, 5, 6, ) } { 4 (3, ) } { 5 "
-      "(1, 3, ) } { 6 (3, ) }");
+      "6 12 { 0 (1, 2, 4, ) } { 1 (0, ) } { 2 (0, 3, 4, 5, ) } { 3 (2, ) } { 4 "
+      "(0, 2, ) } { 5 (2, ) }");
 
   // nodes_ vector is updated post-LCC filtering
   std::vector<int> expected_node_ids = {1, 2, 3, 4, 5, 6};
-  for (Node* node : road_network.nodes_) {
+  for (Node const& node : road_network.graph_) {
     REQUIRE(
         (std::find(
              std::begin(expected_node_ids), std::end(expected_node_ids),
-             node->osm_id_) != std::end(expected_node_ids)) == true);
+             node.osm_id_) != std::end(expected_node_ids)) == true);
   }
 }
