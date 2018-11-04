156668fc41f771735b404d601629cc4f7fd90957
Keyan Pishdadian <kpishdadian@gmail.com>
2018-11-04 02:34:40 -0800

    Cleanup main.cc arg parsing

--- src/main.cc
+++ src/main.cc
@@ -9,11 +9,15 @@ using std::endl;
 int main(int argc, char** argv) {
   if (argc == 1) {
     cout << "Running benchmarks only..." << endl;
+
     run_benchmarks();
-  } else if (argc == 2) {
+  } else if (argc > 1 and argc < 4) {
     cout << "Running in server mode..." << endl;
     int port = atoi(argv[1]);
-    run_server(port);
+    const char* osm_file = "data/seattle.clean.osm";
+    if (argc == 3) { osm_file = argv[2]; }
+
+    run_server(port, osm_file);
   } else {
     cout << "Too many parameters provided" << endl;
     exit(1);
