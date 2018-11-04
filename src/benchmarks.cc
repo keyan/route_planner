32ed72b0de2ac91ad6bb1fee1bfc3b287a835434
Keyan Pishdadian <kpishdadian@gmail.com>
2018-11-04 02:33:58 -0800

    Use std::cout/endl alias in benchmarks for clarity

--- src/benchmarks.cc
+++ src/benchmarks.cc
@@ -9,6 +9,9 @@
 #include "graph_types.h"
 #include "road_network.h"
 
+using std::cout;
+using std::endl;
+
 void run_benchmarks() {
   // Seed random generator
   std::srand(std::time(0));
@@ -16,18 +19,18 @@ void run_benchmarks() {
   RoadNetwork road_network = RoadNetwork();
   road_network.load_from_osm_file("data/burlington.osm");
   // road_network.load_from_osm_file("data/saarland.osm");
-  std::cout << "Number of nodes: " << road_network.graph_.size() << std::endl;
-  std::cout << "Number of edges: " << road_network.num_edges_ << std::endl;
+  cout << "Number of nodes: " << road_network.graph_.size() << endl;
+  cout << "Number of edges: " << road_network.num_edges_ << endl;
 
-  std::cout << "Reducing graph..." << std::endl;
+  cout << "Reducing graph..." << endl;
   std::clock_t begin = std::clock();
   road_network.reduce_to_largest_connected_component();
   std::clock_t end = std::clock();
   double lcc_secs = double(end - begin) / CLOCKS_PER_SEC;
-  std::cout << "LLC reduction: " << lcc_secs << "sec" << std::endl;
+  cout << "LLC reduction: " << lcc_secs << "sec" << endl;
 
-  std::cout << "LCC # of nodes: " << road_network.graph_.size() << std::endl;
-  std::cout << "LCC # of edges: " << road_network.num_edges_ << std::endl;
+  cout << "LCC # of nodes: " << road_network.graph_.size() << endl;
+  cout << "LCC # of edges: " << road_network.num_edges_ << endl;
 
   std::vector<std::pair<Node const&, Node const&>> search_cases;
   for (int i = 0; i < 10; i++) {
@@ -37,9 +40,9 @@ void run_benchmarks() {
     search_cases.emplace_back(origin, target);
   }
 
-  std::cout << std::fixed << std::setprecision(6);
+  cout << std::fixed << std::setprecision(6);
 
-  std::cout << "Running plain dijkstras: " << std::endl;
+  cout << "Running plain dijkstras: " << endl;
   Dijkstras dijkstra = Dijkstras(road_network);
   double total_query_times = 0.0;
   for (auto search_case : search_cases) {
@@ -52,22 +55,21 @@ void run_benchmarks() {
     std::clock_t end = std::clock();
     double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
     total_query_times += elapsed_secs;
-    std::cout << "Elapsed ms: " << elapsed_secs * MS_IN_SEC << std::endl;
+    cout << "Elapsed ms: " << elapsed_secs * MS_IN_SEC << endl;
+    cout << "Settled nodes: " << dijkstra.shortest_path_tree_.size() << endl;
 
-    std::cout << "https://www.google.com/maps/dir/" << origin.lat_ << ","
-              << origin.lng_ << "/" << target.lat_ << "," << target.lng_
-              << std::endl;
-    std::cout << "time: " << (result / MS_IN_SEC) / 60 << "m"
-              << (result / MS_IN_SEC) % 60 << "s" << std::endl;
+    cout << "https://www.google.com/maps/dir/" << origin.lat_ << ","
+         << origin.lng_ << "/" << target.lat_ << "," << target.lng_ << endl;
+    cout << "time: " << (result / MS_IN_SEC) / 60 << "m"
+         << (result / MS_IN_SEC) % 60 << "s" << endl;
   }
 
-  std::cout << std::endl;
-  std::cout << "Average query time ms: "
-            << (total_query_times / search_cases.size()) * MS_IN_SEC
-            << std::endl;
-  std::cout << std::endl;
+  cout << endl;
+  cout << "Average query time ms: "
+       << (total_query_times / search_cases.size()) * MS_IN_SEC << endl;
+  cout << endl;
 
-  std::cout << "Running ALT: " << std::endl;
+  cout << "Running ALT: " << endl;
   ALT alt = ALT(road_network, dijkstra);
   total_query_times = 0.0;
   for (auto search_case : search_cases) {
@@ -80,18 +82,17 @@ void run_benchmarks() {
     std::clock_t end = std::clock();
     double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
     total_query_times += elapsed_secs;
-    std::cout << "Elapsed ms: " << elapsed_secs * MS_IN_SEC << std::endl;
+    cout << "Elapsed ms: " << elapsed_secs * MS_IN_SEC << endl;
+    cout << "Settled nodes: " << dijkstra.shortest_path_tree_.size() << endl;
 
-    std::cout << "https://www.google.com/maps/dir/" << origin.lat_ << ","
-              << origin.lng_ << "/" << target.lat_ << "," << target.lng_
-              << std::endl;
-    std::cout << "time: " << (result / MS_IN_SEC) / 60 << "m"
-              << (result / MS_IN_SEC) % 60 << "s" << std::endl;
+    cout << "https://www.google.com/maps/dir/" << origin.lat_ << ","
+         << origin.lng_ << "/" << target.lat_ << "," << target.lng_ << endl;
+    cout << "time: " << (result / MS_IN_SEC) / 60 << "m"
+         << (result / MS_IN_SEC) % 60 << "s" << endl;
   }
 
-  std::cout << std::endl;
-  std::cout << "Average query time ms: "
-            << (total_query_times / search_cases.size()) * MS_IN_SEC
-            << std::endl;
-  std::cout << std::endl;
+  cout << endl;
+  cout << "Average query time ms: "
+       << (total_query_times / search_cases.size()) * MS_IN_SEC << endl;
+  cout << endl;
 }
